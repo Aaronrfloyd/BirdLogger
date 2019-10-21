935d7a0ae22065a1b5b0537cd24811bca8696afd
Aaron Floyd <aaronrfloyd@gmail.com>
2019-10-21 11:31:17 -0400

    journal

---
+++ BirdLogger.Web.MVC/Controllers/JournalController.cs
@@ -0,0 +1,20 @@
+﻿using BirdLogger.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BirdLogger.Web.MVC.Controllers
+{
+    [Authorize]
+    public class JournalController : Controller
+    {
+        // GET: Journal
+        public ActionResult Index()
+        {
+            var model = new JournalListItem[0];
+            return View();
+        }
+    }
+}

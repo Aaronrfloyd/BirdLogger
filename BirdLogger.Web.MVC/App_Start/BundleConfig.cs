5f3e8dd2d728c6a8b677d8240e78e68786e81276
Aaron Floyd <aaronrfloyd@gmail.com>
2019-10-30 15:28:59 -0400

    almost done

--- BirdLogger.Web.MVC/App_Start/BundleConfig.cs
+++ BirdLogger.Web.MVC/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace BirdLogger.Web.MVC
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }

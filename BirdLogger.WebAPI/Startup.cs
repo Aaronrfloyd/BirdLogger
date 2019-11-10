d81a2da4d326fc02bfde7166eba22463895787bd
Aaron Floyd <aaronrfloyd@gmail.com>
2019-11-10 13:56:40 -0500

    CORS

--- BirdLogger.WebAPI/Startup.cs
+++ BirdLogger.WebAPI/Startup.cs
@@ -12,6 +12,7 @@ namespace BirdLogger.WebAPI
     {
         public void Configuration(IAppBuilder app)
         {
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
             ConfigureAuth(app);
         }
     }

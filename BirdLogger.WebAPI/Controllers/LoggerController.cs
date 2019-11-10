de27a69b239372de989c089a9ff94cc91c57c951
Aaron Floyd <aaronrfloyd@gmail.com>
2019-11-10 12:01:48 -0500

    CRUD

--- BirdLogger.WebAPI/Controllers/LoggerController.cs
+++ BirdLogger.WebAPI/Controllers/LoggerController.cs
@@ -1,4 +1,5 @@
-﻿using BirdLogger.service;
+﻿using BirdLogger.Models;
+using BirdLogger.service;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
@@ -12,12 +13,53 @@ namespace BirdLogger.WebAPI.Controllers
     [Authorize]
     public class LoggerController : ApiController
     {
-        public IHttpActionResult Get()
+        public IHttpActionResult GetAll()
         {
             LoggerService loggerService = CreateLoggerService();
             var loggers = loggerService.GetLogger();
             return Ok(loggers);
         }
+        public IHttpActionResult Get(int id)
+        {
+            LoggerService loggerService = CreateLoggerService();
+            var logger = loggerService.GetLoggerById(id);
+            return Ok();
+        }
+
+        public IHttpActionResult Post(LoggerCreate logger)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateLoggerService();
+
+            if (!service.CreateLogger(logger))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Put(LoggerEdit logger)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateLoggerService();
+
+            if (!service.UpdateLogger(logger))
+                return InternalServerError();
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateLoggerService();
+
+            if (!service.DeleteLogger(id))
+                return InternalServerError();
+            return Ok();
+        }
+
         private LoggerService CreateLoggerService()
         {
             var userId = Guid.Parse(User.Identity.GetUserId());
